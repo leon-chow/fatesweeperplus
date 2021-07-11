6efe58c2136dee49fd583ec2199fd4963fc148ee
Leon <leon.chow1@ontariotechu.net>
2021-07-11 15:07:17 -0400

    Remove logging

--- src/components/gameplay/tile/tile.jsx
+++ src/components/gameplay/tile/tile.jsx
@@ -3,8 +3,6 @@ import React from 'react';
 import './tile.css';
 
 export default function Tile(props, key) {
-    console.log(props);
-    console.log(key);
     return (
         <div className="tile" key={key}> {props.index} </div>
     )
