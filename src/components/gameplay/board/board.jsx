dcc138b737511f51248b9243f05748664505debf
Leon <leon.chow1@ontariotechu.net>
2021-10-17 12:41:17 -0400

    Refactor board class to generate an array and have board return each tile using array properties, add useState

--- src/components/gameplay/board/board.jsx
+++ src/components/gameplay/board/board.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Tile from "../tile/tile";
 import "./board.css";
@@ -6,8 +6,13 @@ import "./board.css";
 const Board = () => {
   const rows = 10;
   const cols = 10;
+  const [board, setBoard] = useState(createBoard());
+
   function tileClicked(row, col) {
     console.log(row, col);
+    const newBoard = board.slice();
+    newBoard[row][col].revealed = true;
+    setBoard(newBoard);
   }
 
   function createBoard() {
@@ -15,23 +20,18 @@ const Board = () => {
     for (let i = 0; i < rows; i++) {
       tileComponents.push([]);
       for (let j = 0; j < cols; j++) {
-        tileComponents[i].push(
-          <Tile
-            tileClicked={() => tileClicked(i, j)}
-            row={i}
-            col={j}
-            key={i * rows + j}
-            revealed={false}
-            hasMine={false}
-            index={i * cols + j}
-          />
-        );
+        tileComponents[i].push({
+          row: i,
+          col: j,
+          key: i * rows + j,
+          revealed: false,
+          hasMine: false,
+          index: i * cols + j,
+        });
       }
     }
     return tileComponents;
   }
-
-  const board = createBoard();
 
   return (
     <div className="board">
@@ -39,7 +39,17 @@ const Board = () => {
         return (
           <div className="row">
             {row.map((tile) => {
-              return <div> {tile} </div>;
+              return (
+                <Tile
+                  tileClicked={() => tileClicked(tile.row, tile.col)}
+                  row={tile.row}
+                  col={tile.col}
+                  key={tile.key}
+                  revealed={tile.revealed}
+                  hasMine={tile.hasMine}
+                  index={tile.index}
+                />
+              );
             })}
           </div>
         );
