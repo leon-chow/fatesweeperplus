b1ec1e4842a721164ccd82c4995377880e2a98eb
Leon <leon.chow1@ontariotechu.net>
2021-10-17 11:54:00 -0400

    Change board and tile classes to functional components

--- src/components/gameplay/board/board.jsx
+++ src/components/gameplay/board/board.jsx
@@ -1,57 +1,51 @@
-import React from 'react';
+import React from "react";
 
-import Tile from '../tile/tile';
-import './board.css'
+import Tile from "../tile/tile";
+import "./board.css";
 
-export default class Board extends React.Component {
+const Board = () => {
+  const rows = 10;
+  const cols = 10;
+  function tileClicked(row, col) {
+    console.log(row, col);
+  }
 
-    constructor() {
-        super();
-
-        this.cols = 10;
-        this.rows = 10;
-
-        this.state = {
-            tileComponents: this.createBoard(),
-        };
-
-        this.tileClicked = this.tileClicked.bind(this);
-        this.createBoard = this.createBoard.bind(this);
+  function createBoard() {
+    let tileComponents = [];
+    for (let i = 0; i < rows; i++) {
+      tileComponents.push([]);
+      for (let j = 0; j < cols; j++) {
+        tileComponents[i].push(
+          <Tile
+            tileClicked={() => tileClicked(i, j)}
+            row={i}
+            col={j}
+            key={i * rows + j}
+            revealed={false}
+            hasMine={false}
+            index={i * cols + j}
+          />
+        );
+      }
     }
+    return tileComponents;
+  }
 
-    tileClicked(row, col) {
-        const updatedTileComponents = [...this.state.tileComponents];
-        updatedTileComponents[row][col] = <Tile tileClicked={() => this.tileClicked(row, col)} row={row} col={col} key={row * this.rows + col} revealed={true} hasMine={false} index={row * this.cols + col}/>;
-        
-        this.setState({
-            tileComponents: updatedTileComponents,
-        });
-    }
+  const board = createBoard();
 
-    createBoard() {
-        let tileComponents = [];
-        for (let i = 0; i < this.rows; i++) {
-            tileComponents.push([]);
-            for (let j = 0; j < this.cols; j++) {
-                tileComponents[i].push(<Tile tileClicked={() => this.tileClicked(i, j)} row={i} col={j} key={i * this.rows + j} revealed={false} hasMine={false} index={i * this.cols + j}/>)
-            }
-        }
-        return tileComponents;
-    }
-
-    render() {
+  return (
+    <div className="board">
+      {board.map((row) => {
         return (
-            <div className="board">
-                { this.state.tileComponents.map((row) => {
-                    return (
-                        <div className="row">
-                            {row.map((tile) => {
-                                return <div> {tile} </div>;
-                            })}
-                        </div>
-                    );
-                })}     
-            </div> 
-        )
-    }
-}
+          <div className="row">
+            {row.map((tile) => {
+              return <div> {tile} </div>;
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Board;
--- src/components/gameplay/tile/tile.jsx
+++ src/components/gameplay/tile/tile.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 import './tile.css';
 
-export default function Tile(props) {
+export const Tile = (props) => {
     return (
         <div onClick={() => props.tileClicked(props.row, props.col)} className={props.revealed === true ? 'tile revealed' : 'tile'}> {props.index} </div>
     );
 }
+
+export default Tile;
