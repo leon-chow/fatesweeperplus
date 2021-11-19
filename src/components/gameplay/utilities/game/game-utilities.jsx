7db9445efd3072f9cc2fb1e845d69d9fd487e87c
Leon <leon.chow1@ontariotechu.net>
2021-11-18 22:46:07 -0500

    Add partial check for mines in one direction

--- src/components/gameplay/utilities/game/game-utilities.jsx
+++ src/components/gameplay/utilities/game/game-utilities.jsx
@@ -4,27 +4,45 @@ export const tileClicked = (board, setBoard, row, col) => {
   }
   const newBoard = board.slice();
   newBoard[row][col].revealed = true;
+  console.log(row, col);
   checkAdjacentTiles(newBoard, setBoard, row, col);
 };
 
 // TODO: Add function
 export const checkAdjacentTiles = (newBoard, setBoard, row, col) => {
-  if (!adjacentTilesHelper(newBoard, row, col)) {
-    setBoard(newBoard);
+  try {
+    upCheck(newBoard, row, col);
+    rightCheck(newBoard, row, col);
+    leftCheck(newBoard, row, col);
+    downCheck(newBoard, row, col);
+  } catch {}
+  setBoard(newBoard);
+};
+
+const upCheck = (newBoard, row, col) => {
+  if (newBoard[row - 1][col] && !newBoard[row - 1][col].hasMine) {
+    newBoard[row - 1][col].revealed = true;
+    upCheck(newBoard, row - 1, col);
   }
 };
 
-const adjacentTilesHelper = (newBoard, row, col) => {
-  upCheck(newBoard, row, col);
-  rightCheck(newBoard, row, col);
-  downCheck(newBoard, row, col);
-  leftCheck(newBoard, row, col);
+const rightCheck = (newBoard, row, col) => {
+  if (newBoard[row][col + 1] && !newBoard[row][col + 1].hasMine) {
+    newBoard[row][col + 1].revealed = true;
+    rightCheck(newBoard, row, col + 1);
+  }
 };
 
-const upCheck = (newBoard, row, col) => {};
+const downCheck = (newBoard, row, col) => {
+  if (newBoard[row - 1][col] && !newBoard[row - 1][col].hasMine) {
+    newBoard[row - 1][col].revealed = true;
+    downCheck(newBoard, row + 1, col);
+  }
+};
 
-const rightCheck = (newBoard, row, col) => {};
-
-const downCheck = (newBoard, row, col) => {};
-
-const leftCheck = (newBoard, row, col) => {};
+const leftCheck = (newBoard, row, col) => {
+  if (newBoard[row][col - 1] && !newBoard[row][col - 1].hasMine) {
+    newBoard[row][col - 1].revealed = true;
+    leftCheck(newBoard, row, col - 1);
+  }
+};
